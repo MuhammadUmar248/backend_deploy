9583e4ce218345393f516e193f105fa6e2a3259a
Muhammad Umar <umershafeeq053@gmail.com>
2025-09-18 18:52:10 +0500

    test

--- main.py
+++ main.py
@@ -5,6 +5,6 @@ app = FastAPI()
 
 app.include_router(router)
 
-# if __name__ == "__main__":
+# if __name__ == "__main__": 
 #     import uvicorn
 #     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
